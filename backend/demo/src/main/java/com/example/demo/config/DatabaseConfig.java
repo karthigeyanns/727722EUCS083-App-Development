2cf010021745a79ae64f324e52589661e5c02759
karthigeyanns <124854653+karthigeyanns@users.noreply.github.com>
2024-08-06 14:27:42 +0530

    Delete backend/demo directory

--- backend/demo/src/main/java/com/example/demo/config/DatabaseConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(basePackages = "com.example.myapp.repository")
-public class DatabaseConfig {
-}
--- backend/demo/src/main/java/com/example/demo/model/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model;
-
-import jakarta.persistence.Entity;
-
-@Entity
-public class Admin extends User {
-    // Additional fields and methods for Admin can be added here if necessary
-}
